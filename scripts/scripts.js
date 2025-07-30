619f71d23dbf20b9ec96d15c14dd0bc5460ae221
Jajer-KSU <jhumph56@students.kennesaw.edu>
2025-07-30 00:51:59 -0400

    Updated website (check description for info)
    Added:
    - better animations
    - coursework section
    - mobile accessibility
    - visually updated entire site

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,22 @@
+// global page load animation //
+window.addEventListener("load", function () {
+  document.body.classList.add("page-loaded");
+});
+
 // shrink nav bar on scroll //
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('mainNavbar');
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  // only shrink screens wider than 600px
+  if (window.innerWidth > 600) {
     if (window.scrollY > 50) {
-        navbar.classList.add('shrink');
+      navbar.classList.add("shrink");
     } else {
-        navbar.classList.remove('shrink');
+      navbar.classList.remove("shrink");
     }
+  } else {
+    navbar.classList.remove("shrink");
+  }
 });
 
 // load coursework
@@ -25,5 +36,43 @@ document.querySelectorAll(".project").forEach((el) => {
     observer.observe(el);
     });
 });
+
+// menu animations
+// Toggle full screen menu
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.querySelector('.menu-links');
+const navLinks = document.querySelectorAll('.nav-links-left, .nav-links-right');
+
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.remove('inactive');
+  menu.classList.toggle('active');
+  menuOpen = menu.classList.contains('active');
+});
+
+// Close menu when link is clicked
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (menuOpen) {
+      menu.classList.remove('active');
+      menu.classList.add('inactive');
+      menuOpen = false;
+    }
+  });
+});
+
+// Optional: close on scroll
+window.addEventListener('scroll', () => {
+  if (menuOpen) {
+    menu.classList.remove('active');
+    menu.classList.add('inactive');
+    menuOpen = false;
+  }
+});
+
+
+
+
 
 
